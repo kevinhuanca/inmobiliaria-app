dcd1e52d5ac3ea326f57f2ed3f64805c31ce4bba
kevinhuanca <kevinhuanca75@gmail.com>
2024-10-28 22:39:55 -0300

    Basic structure

---
+++ app/src/main/java/com/ulp/inmobiliaria/LoginViewModel.java
@@ -0,0 +1,4 @@
+package com.ulp.inmobiliaria;
+
+public class LoginViewModel {
+}
---
+++ app/src/main/java/com/ulp/inmobiliaria/ui/inicio/InicioViewModel.java
@@ -0,0 +1,7 @@
+package com.ulp.inmobiliaria.ui.inicio;
+
+import androidx.lifecycle.ViewModel;
+
+public class InicioViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
